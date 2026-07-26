f04f5df46c10d8efee270b25688fa4c2e431fcae
sd207143-byte <sd207143@gmail.com>
2026-07-26 17:30:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("🎬 Netflix Movies & TV Shows Analysis")
 
@@ -8,3 +9,11 @@ df = pd.read_csv("netflix_titles.csv")
 st.subheader("Netflix Dataset")
 
 st.dataframe(df)
+
+st.subheader("Movies vs TV Shows")
+
+fig, ax = plt.subplots()
+
+df["type"].value_counts().plot(kind="bar", ax=ax)
+
+st.pyplot(fig)
