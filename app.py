387d489eb71410af9197de614198a8797e2d32ae
sd207143-byte <sd207143@gmail.com>
2026-07-26 17:29:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Netflix Movies & TV Shows Analysis")
+st.title("🎬 Netflix Movies & TV Shows Analysis")
 
-st.write("Welcome to my Data Science Project!")
+df = pd.read_csv("netflix_titles.csv")
+
+st.subheader("Netflix Dataset")
+
+st.dataframe(df)
