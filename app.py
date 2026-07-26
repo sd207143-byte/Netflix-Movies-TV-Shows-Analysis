6d48080d77a60abac3bcc8a0e6fbaf1dc96ba5aa
sd207143-byte <sd207143@gmail.com>
2026-07-26 17:34:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,48 @@ fig, ax = plt.subplots()
 df["type"].value_counts().plot(kind="bar", ax=ax)
 
 st.pyplot(fig)
+st.subheader("Top 10 Countries")
+
+fig, ax = plt.subplots()
+
+df["country"].value_counts().head(10).plot(kind="bar", ax=ax)
+
+st.pyplot(fig)
+st.subheader("Top 10 Ratings")
+
+fig, ax = plt.subplots()
+
+df["rating"].value_counts().head(10).plot(kind="bar", ax=ax)
+
+st.pyplot(fig)
+st.subheader("Content Released by Year")
+
+fig, ax = plt.subplots()
+
+df["release_year"].value_counts().sort_index().plot(kind="line", ax=ax)
+
+st.pyplot(fig)
+st.subheader("Top 10 Genres")
+
+fig, ax = plt.subplots()
+
+df["listed_in"].value_counts().head(10).plot(kind="bar", ax=ax)
+
+st.pyplot(fig)
+st.subheader("Top 10 Durations")
+
+fig, ax = plt.subplots()
+
+df["duration"].value_counts().head(10).plot(kind="bar", ax=ax)
+
+st.pyplot(fig)
+st.subheader("Content Added to Netflix by Year")
+
+df["date_added"] = pd.to_datetime(df["date_added"], errors="coerce")
+df["Year Added"] = df["date_added"].dt.year
+
+fig, ax = plt.subplots()
+
+df["Year Added"].value_counts().sort_index().plot(kind="line", ax=ax)
+
+st.pyplot(fig)
